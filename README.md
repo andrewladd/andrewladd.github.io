8b7627ffde246485acea76b8d75d502c4a27dc7b
Andrew Ladd <andrewblakeladd@live.com>
2015-05-06 19:59:57 -0400

    Redoing entire sitecl

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # andrewladd.github.io
 Personal GitHub Pages site. Cause everyone else was. And it was easy. And webdev!
 
-
-Currently terrible. Pay no mind. Please?
+andrewladd.github.io
---
+++ js/script.js
@@ -0,0 +1,50 @@
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+function change() {
+	var allinfo = document.getElementsByTagName("p");
+	var showninfo;
+	var newinfo;
+	
+	//get the p that is displayed
+	for (var i = 0; i < allinfo.length; i++) {
+		if (allinfo[i].hidden == false) {
+			showninfo = allinfo[i];
+			break;
+		}
+	}
+	
+	var shownID = showninfo.id;
+	
+	switch (shownID) {
+		case "info9":
+			newinfo = document.getElementById('info1');
+			break;
+		case "info1":
+			newinfo = document.getElementById('info2');
+			break;
+		case "info2":
+			newinfo = document.getElementById('info3');
+			break;
+		case "info3":
+			newinfo = document.getElementById('info4');
+			break;
+		case "info4":
+			newinfo = document.getElementById('info5');
+			break;
+		case "info5":
+			newinfo = document.getElementById('info6');
+			break;
+		case "info6":
+			newinfo = document.getElementById('info7');
+			break;
+		case "info7":
+			newinfo = document.getElementById('info8');
+			break;
+		case "info8":
+			newinfo = document.getElementById('info9');
+			break;
+	}
+	
+	//hide showninfo & display newinfo
+	showninfo.hidden = true;
+	newinfo.hidden = false;
+}
